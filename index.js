650e354f89f38b14fd5df85a5998f9387a4c170c
Matt Natan <mathewsmaps@gmail.com>
2023-10-14 23:21:18 +0200

    Superheroes name gerator

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 //Tape npm init on cmd
 //installing npm packages we use npm install<name of package>
 
-import generateName from "sillyname";
+// import generateName from "sillyname";
 
-var generateName = require('sillyname');
-var sillyName = generateName();
+// var generateName = require('sillyname');
+// var sillyName = generateName();
 
-console.log("My name is "+sillyName);
+// console.log("My name is "+sillyName);
+
+import superheroes from "superheroes";
+const name = superheroes.random();
+
+console.log("My name is "+name+"! ");
