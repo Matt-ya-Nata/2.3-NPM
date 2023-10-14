b82283255a63d370f9d7411288c0cfecc5e3bb7f
Matt Natan <mathewsmaps@gmail.com>
2023-10-14 23:00:55 +0200

    Running

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 //Tape npm init on cmd
 //installing npm packages we use npm install<name of package>
+
+var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log("My name is "+sillyName);
